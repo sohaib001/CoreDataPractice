5577607db509ef7d7160507a56f919a50cc827a8
Sohaib Muhammad <sohaib.muhammad@coeus-solutions.de>
2013-03-18 11:12:12 +0500

    Initial Commit

---
+++ CoreDataPractice2/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreDataPractice2
+//
+//  Created by Sohaib Muhammad on 18/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
